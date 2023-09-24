6ecdb55d4b7b6adff86716a41b8953ad29aac3a9
Didier Clabaut <didier.clabaut@integrationdesigners.com>
2023-09-24 19:21:12 +0200

    Typo correctie
    Signed-off-by: Didier Clabaut <didier.clabaut@integrationdesigners.com>

--- src/test/java/ex02_product_description_OPL/ProductTest.java
+++ src/test/java/ex02_product_description_OPL/ProductTest.java
@@ -6,37 +6,37 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 
 public class ProductTest {
     @Test
-    void commonElements_allNull() {
+    void productDescription_allNull() {
         Product p = new Product("empty", null, null);
         String result = p.productDescription();
         assertEquals("", result);
     }
     @Test
-    void commonElements_sizeSet_percentageNull() {
+    void productDescription_sizeSet_percentageNull() {
         Product p = new Product("empty", 25, null);
         String result = p.productDescription();
         assertEquals(" • 25CL", result);
     }
     @Test
-    void commonElements_sizeNull_percentageSet() {
+    void productDescription_sizeNull_percentageSet() {
         Product p = new Product("empty", null, 8.0);
         String result = p.productDescription();
         assertEquals(" • 8%", result);
     }
     @Test
-    void commonElements_allSet() {
+    void productDescription_allSet() {
         Product p = new Product("empty", 33, 6.0);
         String result = p.productDescription();
         assertEquals(" • 33CL 6%", result);
     }
     @Test
-    void commonElements_percentageWithDecimal() {
+    void productDescription_percentageWithDecimal() {
         Product p = new Product("empty", 33, 6.3);
         String result = p.productDescription();
         assertEquals(" • 33CL 6.3%", result);
     }
     @Test
-    void commonElements_percentageWithTooMuchDecimals() {
+    void productDescription_percentageWithTooMuchDecimals() {
         Product p = new Product("empty", 33, 6.333);
         String result = p.productDescription();
         assertEquals(" • 33CL 6.3%", result);
