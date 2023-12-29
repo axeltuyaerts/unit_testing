b20f6877b897c68d027707dcf6ac11a3f51424f2
axelt <r0784549@student.thomasmore.be>
2023-12-29 12:09:18 +0100

    exercise sumOf3SmallestInts

---
+++ src/test/java/ex01_arrays/ArrayOfIntsSumOf3SmallestInts.java
@@ -0,0 +1,34 @@
+package ex01_arrays;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+public class ArrayOfIntsSumOf3SmallestInts {
+    private final ArrayOfInts arrayOfInts = new ArrayOfInts();
+
+    @Test
+    void happyflow() {
+        int result = arrayOfInts.sumOf3SmallestInts(new int[]{1, 1, 1, 5, 7, 6, 4});
+        assertEquals(3, result);
+    }
+
+    @Test
+    void null_returns0() {
+        int result = arrayOfInts.sumOf3SmallestInts(null);
+        assertEquals(0, result);
+    }
+
+    @Test
+    void empty_array() {
+        int result = arrayOfInts.sumOf3SmallestInts(new int[]{});
+        assertEquals(0, result);
+    }
+
+    @Test
+    void small_array() {
+        int result = arrayOfInts.sumOf3SmallestInts(new int[]{1, 1});
+        assertEquals(2, result);
+    }
+}
